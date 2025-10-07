fb3d1a4709010b30dcf425905a4f067d677cf341
tpqms2061 <tpqms2061@naver.com>
2025-10-07 19:12:56 +0900

    oAuth2 Login2

--- backend/src/main/java/com/ssh/backend/security/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/ssh/backend/security/OAuth2SuccessHandler.java
@@ -37,7 +37,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String name = oAuth2User.getAttribute("name");
         String avatarUrl = oAuth2User.getAttribute("picture");
 
-        //깃허브
+        //깃허브에는 email 항목이 없을수도 있고 login 항목은 무조건 존재함  그래서 login@github.local형식으로 나타냄
         if (email == null && oAuth2User.getAttribute("login") != null) {
             email = oAuth2User.getAttribute("login") + "@github.local";
             avatarUrl = oAuth2User.getAttribute("avatar_url");
@@ -69,16 +69,20 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String accessToken = jwtService.generateToken(user);
         String refreshToken = jwtService.generateRefreshToken(user);
 
-        String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl + "/oauth2/callback")
+        //백엔드가 프론트엔드로 토큰을 전달하기 위한 리다이렉트 URL을 조립하는 장치
+        String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl + "/oauth2/callback") //리다이렉트할 페이지 경로 지정
                 .queryParam("token", accessToken)
-                .queryParam("refreshToken", refreshToken)
-                .build().toUriString();
+                .queryParam("refreshToken", refreshToken)  //accessToken과  refreshToken을 리다이렉트 경로에 포함
+                .build().toUriString();  //문자열형태로 완성
 
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        //사용자를 해당 URL로 리다렉트 시킴
 
     }
+    //username 고유성 보장하기위한 장치
     private String generateUsername(String email) {
         String baseUsername = email.split("@")[0].toLowerCase().replaceAll("[^a-z0-9]", "");
+        //영문자와 숫자를 제외하고 삭제 =>  abd.def => abddef로 표현
         String username = baseUsername;
         int counter = 1;
 
